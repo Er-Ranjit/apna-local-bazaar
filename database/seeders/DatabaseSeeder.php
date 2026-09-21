68608d728b5ac6fe5430832cdf93f573213022e3
Er-Ranjit <ranjitkumar728064@gmail.com>
2026-09-21 13:40:48 +0530

    Remove product seeder to preserve product images

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             VendorSeeder::class,
-            ProductSeeder::class,
         ]);
     }
 }
